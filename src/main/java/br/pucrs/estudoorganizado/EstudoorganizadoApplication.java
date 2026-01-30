51936efa55bd0024cb0b501952b07e294216f2e6
Berlezi <jessica.berlezi@gmail.com>
2026-01-29 22:08:19 -0300

    spring initilizr

---
+++ src/main/java/br/pucrs/estudoorganizado/EstudoorganizadoApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.estudoorganizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoorganizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoorganizadoApplication.class, args);
+	}
+
+}
