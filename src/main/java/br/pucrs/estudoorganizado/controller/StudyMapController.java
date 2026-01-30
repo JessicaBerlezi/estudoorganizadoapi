013c7b5d26800424ae67acd0a316e08180a12b58
Berlezi <jessica.berlezi@gmail.com>
2026-01-30 15:15:08 -0300

    POST new subject API

--- src/main/java/br/pucrs/estudoorganizado/controller/StudyMapController.java
+++ src/main/java/br/pucrs/estudoorganizado/controller/StudyMapController.java
@@ -1,20 +1,30 @@
 package br.pucrs.estudoorganizado.controller;
 
+import br.pucrs.estudoorganizado.controller.dto.InsertSubjectDTO;
 import br.pucrs.estudoorganizado.controller.dto.StudyMapsDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/mock-api/study-map")
 public class StudyMapController {
 
+    private static final Logger logger = LoggerFactory.getLogger(StudyMapController.class);
+
     @GetMapping
     public StudyMapsDTO getStudyMaps() {
         return MocksFactory.createStudyMapsMock();
-
     }
 
+    @PostMapping("/subject")
+    public ResponseEntity<Integer> postSubject(@Valid @RequestBody InsertSubjectDTO newSubject){
+        logger.info("New subject: {}", newSubject.toLogString());
+        return ResponseEntity.ok(200);
+    }
 }
 
 
---
+++ src/main/java/br/pucrs/estudoorganizado/controller/dto/InsertSubjectDTO.java
@@ -0,0 +1,33 @@
+package br.pucrs.estudoorganizado.controller.dto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InsertSubjectDTO {
+    @NotBlank(message = "Nome da disciplina é obrigatória")
+    @Size(max = 100, message = "Nome da disciplina deve ter no máximo 100 caracteres")
+    public String description;
+
+    @Size(max = 250, message = "Anotação deve ter no máximo 250 caracteres")
+    public String annotation;
+
+    @Valid
+    public List<InsertTopicDTO> topics;
+
+    public String toLogString() {
+        String topicsLog = (topics == null || topics.isEmpty())
+                ? "[]"
+                : topics.stream()
+                .map(InsertTopicDTO::toLogString)
+                .collect(Collectors.joining(", ", "[", "]"));
+
+        return String.format(
+                "description='%s', annotation=%s, topics=%s",
+                description,
+                annotation != null && !annotation.isBlank(),
+                topicsLog
+        );
+    }
+}
