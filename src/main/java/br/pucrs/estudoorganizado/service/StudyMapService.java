a8d8702185f6509a0bafe00917b1b68d80e8fd52
Berlezi <jessica.berlezi@gmail.com>
2026-02-12 21:14:39 -0300

    Refatorações pequenas

--- src/main/java/br/pucrs/estudoorganizado/service/StudyMapService.java
+++ src/main/java/br/pucrs/estudoorganizado/service/StudyMapService.java
@@ -1,16 +1,12 @@
 package br.pucrs.estudoorganizado.service;
 
 import br.pucrs.estudoorganizado.controller.dto.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class StudyMapService {
-
-    private static final Logger logger = LoggerFactory.getLogger(StudyMapService.class);
 
     private final SubjectService service;
 
