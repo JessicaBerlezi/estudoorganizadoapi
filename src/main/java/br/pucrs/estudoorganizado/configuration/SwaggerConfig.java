3b2bd4f2738d34c4bfeace299cb87e4ec32e281d
Berlezi <jessica.berlezi@gmail.com>
2026-05-02 18:06:13 -0300

    fix swagger https

--- src/main/java/br/pucrs/estudoorganizado/configuration/SwaggerConfig.java
+++ src/main/java/br/pucrs/estudoorganizado/configuration/SwaggerConfig.java
@@ -1,16 +1,24 @@
 package br.pucrs.estudoorganizado.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://estudoorganizadoapi.up.railway.app");
+
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info()
                         .title("Estudo Organizado API")
                         .version("v1")
